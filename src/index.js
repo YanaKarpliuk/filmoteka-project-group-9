b4b109a9c6e63b70568f10df65ca717526da9ea9
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-09-11 22:42:15 +0300

    modal-student.js#2.3

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ApiQuery from './js_components/api_query';
 import headerHeight from "./js-components/header-height";
 import { renderTrendMovies } from './trendMoviesCards';
 renderTrendMovies();
-import students from './modal-student'
+import modalstudent from './modal-student.js';
--- src/modal-student.js
+++ src/modal-student.js
@@ -1,56 +1,30 @@
-const {
-    footerLightbox,
-    footerLightboxOpenBtn,
-    footerLightboxCloseBtn,
-    body,
-  } = refs;
-  
-  footerLightboxOpenBtn.addEventListener('click', onOpenModal);
-  
-  
-  function onOpenModal(e) {
-    e.preventDefault();
-    window.addEventListener('keydown', onEscBtnPress);
-    footerLightboxCloseBtn.addEventListener('click', onCloseModal);
-    footerLightbox.addEventListener('click', onLightBoxOverlayClick);
-    footerLightbox.classList.add('is-open');
-    body.classList.add('is-open');
-    appendTeamCardsMarkup(students);
-  }
-  
-  function onCloseModal() {
-    footerLightboxCloseBtn.removeEventListener('click', onCloseModal);
-    footerLightbox.removeEventListener('click', onLightBoxOverlayClick);
-    window.removeEventListener('keydown', onEscBtnPress);
-    footerLightbox.classList.remove('is-open');
-    body.classList.remove('is-open');
-  }
-  
-  
- 
-  
-  function onLightBoxOverlayClick(e) {
-    if (e.currentTarget === e.target) {
-      onCloseModal();
+const refs = {
+    openModalBtn:document.querySelector('[data-action="open-modal"]'),
+    closeModalBtn:document.querySelector('[data-action="close-modal"]'),
+    backdrop:document.querySelector('.js-backdrop')
+};
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onOpenModal(){
+    window.addEventListener('keydown', onEscKeyPress);
+    document.body.classList.add('show-modal');
+}
+function onCloseModal(){
+    window.removeEventListener('keydown', onEscKeyPress);
+    document.body.classList.remove('show-modal');
+}
+function onBackdropClick(event){
+    if(event.currentTarget ===event.target){
+        onCloseModal() ;
     }
-  }
-  
-  function onEscBtnPress(e) {
-    if (e.code === 'Escape') {
-      onCloseModal();
+}
+function onEscKeyPress(event){
+    const ESC_KEY_CODE ='Escape';
+
+    if(event.code === ESC_KEY_CODE){
+        onCloseModal(); 
     }
-  }
-  // (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector("[data-modal-open]"),
-//       closeModalBtn: document.querySelector("[data-modal-close]"),
-//       modal: document.querySelector("[data-modal]"),
-//     };
-  
-//     refs.openModalBtn.addEventListener("click", toggleModal);
-//     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-//     function toggleModal() {
-//       refs.modal.classList.toggle("is-hidden");
-//     }
-//   })();
+}
